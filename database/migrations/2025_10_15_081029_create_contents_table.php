c3c5cf34bf032f872ef75002c71df804fad8abc2
1073500 <1073500@hr.nl>
2025-10-15 11:03:39 +0200

    Migrations gemaakt

---
+++ database/migrations/2025_10_15_081029_create_contents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('name');
+            $table->string('constellation');
+            $table->tinyText('description');
+            $table->string('type');
+            $table->string('image_url');
+            $table->string('country');
+            $table->string('city');
+            $table->string('town');
+            $table->string('province');
+            $table->date('date');
+            $table->string('views')->default(0);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
