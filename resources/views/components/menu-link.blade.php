0308ebc184c403e250c44d2978f532a442b38a7a
1073500 <1073500@hr.nl>
2025-10-19 02:57:32 +0200

    footer

--- resources/views/components/menu-link.blade.php
+++ resources/views/components/menu-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes = ($active ?? false)
                 ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-                : 'bg-gray-900 p-2 m-2 rounded-full inline-flex items-center px-6 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-200 focus:outline-none focus:text-gray-700 dark:focus:text-gray-200 focus:bg-white/10 transition duration-150 ease-in-out';
+                : 'bg-gray-900 p-2 m-2 rounded-full inline-flex items-center px-6 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:duration-300 ease-in hover:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-200 focus:outline-none';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="heading" ><h1>Contact page</h1></x-slot>
     <x-slot name="title"><h1>Hello Merquery contactpage! </h1></x-slot>
-    <x-slot name="footer">
+    <x-slot name="test">
         <ul>
             <li>
                 address: Wijnhaven 107, Rotterdam
